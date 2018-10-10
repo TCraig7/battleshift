ec4526559d6ecb000a35d2cf555da53b15221918
Tara Craig <tara.craig7@gmail.com>
2018-10-10 15:06:35 -0600

    Add VCR gem stuff.

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ end
 
 group :test do
   gem 'simplecov'
+  gem 'vcr'
+  gem 'webmock'
 end
 
 group :development do
--- spec/features/visitor_can_see_all_users_spec.rb
+++ spec/features/visitor_can_see_all_users_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 feature 'visitor can see all users' do
   scenario 'can see all users' do
-    visit '/users'
+    VCR.use_cassette("users_index") do
+      visit '/users'
 
-    expect(page).to have_content("Josiah Bartlet")
-    expect(page).to have_content("Jon Tester")
+      expect(page).to have_content("Josiah Bartlet")
+      expect(page).to have_content("Jon Tester")
+    end
   end
 end
