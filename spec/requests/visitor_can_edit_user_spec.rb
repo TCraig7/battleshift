967c151c6df2524453e4104e5405be82eab1401e
Tara Craig <tara.craig7@gmail.com>
2018-10-11 11:14:27 -0600

    Add endpoint for patch in users controllers, spec.

---
+++ spec/requests/visitor_can_edit_user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'users api endpoints' do
+  context 'PATCH /api/v1/users/:id' do
+    it "allows a visitor to edit a user email" do
+      user_1, user_2 = create_list(:user, 2)
+      email = "jbartlet22@gmail.com"
+
+      patch "/api/v1/users/#{user_1.id}", params: {email: email}
+      expect(response.status).to be 200
+      expect(User.find(user_1.id).email).to eq(email)
+    end
+  end
+end
