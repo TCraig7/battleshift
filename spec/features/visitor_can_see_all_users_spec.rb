6a2ff6443d3d00a1a6536391794269e89faf1db5
Tara Craig <tara.craig7@gmail.com>
2018-10-11 11:48:54 -0600

    Add test to edit user email view.

--- spec/features/visitor_can_see_all_users_spec.rb
+++ spec/features/visitor_can_see_all_users_spec.rb
@@ -9,4 +9,14 @@ feature 'visitor can see all users' do
       expect(page).to have_content("Jon Tester")
     end
   end
+
+  scenario 'can edit a user email' do
+    VCR.use_cassette("users_edit_index") do
+      visit '/users'
+
+      click_on "Edit"
+
+      expect(path).to eq("/users/1/edit")
+    end
+  end
 end
