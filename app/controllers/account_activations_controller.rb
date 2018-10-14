accda8726b107789fe025d0040f96c91634864c2
Tara Craig <tara.craig7@gmail.com>
2018-10-14 17:11:16 -0600

    Add activation email stuff. ATP.

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,6 +1,8 @@
 class AccountActivationsController < ApplicationController
 
   def edit
-
+    user = User.find_by_email(params[:id])
+    user.activate_account
+    flash[:success] = "Account activated!"
   end
 end
--- spec/mailers/user_sees_email_spec.rb
+++ spec/mailers/user_sees_email_spec.rb
@@ -1,22 +1,22 @@
 require "rails_helper"
 
-RSpec.describe UserNotifier, type: :mailer do
+RSpec.describe ActivationMailer, type: :mailer do
   describe "activate user account" do
     it "renders the response headers" do
 			user = create(:user)
-      mail = UserNotifier.activate(user)
+      mail = ActivationMailer.account_activation(user)
 
-      expect(mail.subject).to eq("Confirm Your Account")
-      expect(mail.to).to eq(["jtester@example.com"])
+      expect(mail.subject).to eq("Activation Email for #{user.email}")
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["no-reply@example.com"])
     end
 
     it "renders the response body" do
 			user = create(:user)
-			mail = UserNotifier.activate(user)
+			mail = ActivationMailer.account_activation(user)
 
-      expect(mail.body.encoded).to have_content("Click here to activate your account")
-      expect(mail.body.encoded).to have_content("API Key: ")
+      expect(mail.body.encoded).to have_content("Click here to activate your account:")
+      expect(mail.body.encoded).to have_content("API key")
      end
    end
 end
