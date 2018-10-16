61de6bf2f019159312f2f0006de69c564fc1a92b
Harper Bellows <harper.bellows@gmail.com>
2018-10-16 07:32:54 -0600

    add solution for first spec harness failure

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class GamesController < ActionController::API
+      
       def show
         if game = Game.find_by(id: params[:id])
           render json: game
@@ -8,6 +9,24 @@ module Api
           render :status => 400
         end
       end
+      
+      def create
+        game = Game.new(game_attributes)
+        if game.save
+            render json: game, status: 200
+        else
+          render status: 400
+        end
+      end
+
+      private 
+        def game_attributes
+          { 
+            player_1_board: Board.new(4),
+            player_2_board: Board.new(4)
+          }
+        end
     end
   end
 end
+
