27afcf483222c2f6cd9eaa11506d5e730304a553
Tara Craig <tara.craig7@gmail.com>
2018-10-16 16:36:07 -0600

    Solve issues in RSpec through line 20.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -8,6 +8,14 @@ module Api
           render :status => 400
         end
       end
+
+      def create
+        board_1 = Board.new(4)
+        board_2 = Board.new(4)
+        game = Game.create( player_1_board: board_1,
+                            player_2_board: board_2)
+        render json: game
+      end
     end
   end
 end
--- app/services/values/board.rb
+++ app/services/values/board.rb
@@ -1,10 +1,12 @@
 class Board
   attr_reader :length,
-              :board
+              :board, 
+              :count
 
   def initialize(length)
     @length = length
     @board = create_grid
+    @count = 0
   end
 
   def get_row_letters
@@ -203,4 +205,3 @@ class Board
     get_column(coordinate) == "1"
   end
 end
-
