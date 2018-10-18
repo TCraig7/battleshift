9f18b34036b998225dee5e3c03faf2f5be793f9c
Tara Craig <tara.craig7@gmail.com>
2018-10-17 18:03:00 -0600

    Complete spec harness through line 75.

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -6,9 +6,12 @@ module Api
           game = Game.find_by(id: params[:game_id])
 
           turn_processor = TurnProcessor.new(game, params[:shot][:target], request.headers["X-API-Key"])
-
-          turn_processor.run!
-          render json: game, message: turn_processor.message
+          if turn_processor.check_current_player
+            turn_processor.run!
+            render json: game, message: turn_processor.message
+          else
+            render json: game, :status => 400, message: turn_processor.message
+          end
         end
       end
     end
